3205d41cddc830accf44965dc4db4e3803f1e729
Vitalii Paprotskyi <vetalpaprotsky@gmail.com>
2019-01-18 17:12:07 +0200

    add doc for sessions controller

---
+++ spec/acceptance/api/v1/sessions_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'Sessions', type: :acceptance do
+  let(:current_user) { create(:user) }
+
+  post '/api/v1/users/sign_in' do
+    with_options scope: :user, in: :body do
+      parameter :email
+      parameter :password
+    end
+
+    example 'Singing in a user' do
+      do_request(user: { email: current_user.email, password: '12345678' })
+
+      expect(status).to eq(200)
+    end
+  end
+
+  delete '/api/v1/users/sign_out' do
+    add_auth_parameters(optional: false)
+    before { authenticate!(current_user) }
+
+    example 'Singing out the current user' do
+      do_request
+
+      expect(status).to eq(200)
+    end
+  end
+end
